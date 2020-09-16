234d39a3e67d6c7ed35e8f26b66aa7c6bf6e1a60
250mon <lambki@naver.com>
2020-09-16 18:03:33 +0900

    opt10081

--- get_codelist_by_market.py
+++ get_codelist_by_market.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QAxContainer import *
 from PyQt5.QtCore import *
 
+
 class Kiwoom(QAxWidget):
     def __init__(self):
         super().__init__()
@@ -14,6 +15,7 @@ class Kiwoom(QAxWidget):
 
     def _set_signal_slots(self):
         self.OnEventConnect.connect(self._event_connect)
+        self.OnReceiveTrData.connect(self._receive_tr_data)
 
     def comm_connect(self):
         self.dynamicCall("CommConnect()")
@@ -33,10 +35,59 @@ class Kiwoom(QAxWidget):
         code_list = code_list.split(';')
         return code_list[:-1]
 
+    def get_master_code_name(self, code):
+        code_name = self.dynamicCall("GetMasterCodeName(QString", code)
+        return code_name
+
+    def set_input_value(self, id, value):
+        self.dynamicCall("SetInputValue(QString, QString", id, value)
+
+    def comm_rq_data(self, rqname, trcode, next, screen_no):
+        self.dynamicCall("CommRqData(QString, QString, int, QString", rqname, trcode, next, screen_no)
+        self.tr_event_loop = QEventLoop()
+        self.tr_event_loop.exec_()
+
+    def comm_get_data(self, tr_code, real_type, field_name, index, item_name):
+        ret = self.dynamicCall("GetCommData(QString, QString, QString, int, QString", tr_code, real_type, field_name,
+                               index, item_name)
+        return ret.strip()
+
+    def _get_repeat_cnt(self, trcode, rqname):
+        ret = self.dynamicCall("GetRepeatCnt(QString, QString", trcode, rqname)
+        return ret
+
+    def _receive_tr_data(self, screen_no, rqname, trcode, record_name, next, unused1, unused2, unused3, unusde4):
+        if next == '2':
+            self.remained_data = True
+        else:
+            self.remained_data = False
+
+        if rqname == "opt10081_req":
+            self._opt10081(rqname, trcode)
+
+        try:
+            self.tr_event_loop.exit()
+        except AttributeError:
+            pass
+
+    def _opt10081(self, rqname, trcode):
+        data_cnt = self._get_repeat_cnt(trcode, rqname)
+
+        for i in range(data_cnt):
+            date = self._comm_get_data(trcode, "", rqname, i, "일자")
+            open = self._comm_get_data(trcode, "", rqname, i, "시가")
+            high = self._comm_get_data(trcode, "", rqname, i, "고가")
+            low = self._comm_get_data(trcode, "", rqname, i, "저가")
+            close = self._comm_get_data(trcode, "", rqname, i, "현재가")
+            volume = self._comm_get_data(trcode, "", rqname, i, "거래량")
+            print(date, open, high, low, close, volume)
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     kiwoom = Kiwoom()
     kiwoom.comm_connect()
     code_list = kiwoom.get_code_list_by_market('10')
     for code in code_list:
-        print(code, end=" ")
+        code_name = kiwoom.get_master_code_name(code)
+        print(code_name, end=" ")
